f2149824adc4c0fca748e6271774df38c3dfab78
EvgeniyaDanilovich <evgeniyadanilovich@gmail.com>
2023-05-17 14:17:09 +0300

    feat: add router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import './styles/index.scss';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from 'app/providers/router';
 
 const App = () => {
     const { theme, switchTheme } = useTheme();
@@ -14,12 +13,7 @@ const App = () => {
             <button onClick={switchTheme}>Theme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
-            <Suspense fallback={'Loading...'}>
-                <Routes>
-                    <Route path={'/'} element={<MainPage />} />
-                    <Route path={'/about'} element={<AboutPage />} />
-                </Routes>
-            </Suspense>
+           <AppRouter />
         </div>
     );
 };
--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const AboutPage = () => {
+    const date = new Date;
+    const currentTime = `${date.getFullYear()}, ${date.getMonth()},  ${date.getDate()}`
+
     return (
-        <div>
+        <div className={'time'}>
             AboutPage
+            <div>{currentTime}</div>
         </div>
     );
 };
