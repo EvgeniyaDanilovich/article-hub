65214ff92cbeeb4ce6cbff89ec38841878b411a3
EvgeniyaDanilovich <evgeniyadanilovich@gmail.com>
2023-05-24 17:06:45 +0300

    feat: add not found route, style loader

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './NotFoundPage.module.scss'
+import { useTranslation } from 'react-i18next';
+
+interface NotFoundPageProps{
+    className?: string;
+}
+
+export const NotFoundPage: React.FC<NotFoundPageProps> = ({className}) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.NotFoundPage, {}, [])}>
+            {t('page not found')}
+        </div>
+    );
+};
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Loader.scss'
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from '*.scss';
+
+interface LoaderProps{
+    className?: string;
+}
+
+export const Loader: React.FC<LoaderProps> = ({className}) => {
+    return (
+        <div className={classNames('lds-ellipsis', {}, [])}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
