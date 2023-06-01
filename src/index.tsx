cbadfd6ed8309fef0a1b6c059b4c225aa87b65f1
EvgeniyaDanilovich <evgeniyadanilovich@gmail.com>
2023-06-01 13:02:17 +0300

    add json-server

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
 import 'shared/config/i18n/i18n';
+import './app/styles/index.scss';
 
 render(
     <StoreProvider>
